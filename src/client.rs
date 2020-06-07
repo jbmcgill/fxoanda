92b1079727502f669004789fe0cda310ea61266c
James McGill <jbmcgill@gmail.com>
2020-06-07 00:46:31 -0700

    #3 add docs about client being synchronous and Client.host does not include https prefix

--- src/client.rs
+++ src/client.rs
@@ -1,9 +1,14 @@
 use std::error::Error;
 
+/// HTTP Client used to call the OANDA web services.
 #[derive(Debug)]
 pub struct Client {
+  /// The reqwest object to use. Note that this is a synchronous client
+  /// that cannot be used in async code.
   pub reqwest: reqwest::Client,
+  /// OANDA host to use (do not include https://)
   pub host: String,
+  /// OANDA API key
   pub authentication: String,
 } 
 
